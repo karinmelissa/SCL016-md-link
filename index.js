08b9e2a14343062da67baafdf2108837e502a924
karinmelissa <karin.escobar@usach.cl>
2021-05-24 16:10:30 -0400

    Feat: validate Links

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const mdLinks = require('./lib/mdLinks.js');
+const findMdLinks = require('./lib/mdLinks.js');
 const pathJS = require('path');
 
-/* module.exports = () => {
+//getting the route & validate request from the console 
+let routeFile = process.argv[2];
+let validateLinks = process.argv[3];
+//resolves a sequence of paths or path segments into an absolute path.
+routeFile = pathJS.resolve(routeFile);
+//normalizes the given pathfixing writtings errors 
+routeFile = pathJS.normalize(routeFile);
 
-  // ...
-}; */
+findMdLinks.mdLinks(routeFile,validateLinks);
