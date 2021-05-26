459d32426d5ac85cfde4b8733d6a8c583f07f731
karinmelissa <karin.escobar@usach.cl>
2021-05-26 11:50:35 -0400

    Feat: CLI --validate & --stats

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+#!/usr/bin/env node
 const findMdLinks = require('./lib/mdLinks.js');
 const path = require('path');
+const { program } = require('commander');
+const { link } = require('fs');
 
 //getting the route & validate request from the console 
 let routeFile = process.argv[2];
@@ -9,4 +12,47 @@ routeFile = path.resolve(routeFile);
 //normalizes the given path, fixing writtings errors 
 routeFile = path.normalize(routeFile);
 
-findMdLinks.mdLinks(routeFile,validateLinks);
+program
+  .option('--validate', 'validate links')
+  .option('--stats', 'show statistics instead of list')
+program.parse(process.argv);  
+
+const options = program.opts();
+if(!options.validate & !options.stats){
+  findMdLinks.mdLinks(routeFile).then((links)=>{
+    console.log(links);
+  });
+}
+else if(options.validate & !options.stats){
+  findMdLinks.mdLinks(routeFile,true).then((links)=>{
+    console.log(links);
+  });
+}
+else if(!options.validate & options.stats){
+  findMdLinks.mdLinks(routeFile, true).then((links)=>{
+    const getStats = showStats(links)
+    console.log(getStats)
+  })
+}
+else if(options.validate & options.stats){
+  findMdLinks.mdLinks(routeFile, true).then((links)=>{
+    const getStats = showStats(links)
+    console.log(links)
+    console.log(getStats)
+  })
+}
+
+const showStats = (linksArray)=>{
+  const linksStats = {
+    Total: 0,
+    //Unique: 0, que son los unique?
+    Broken: 0,
+  }
+  linksStats.Total = linksArray.length;
+  linksArray.forEach((e) => {
+    if(e.ok=='fail'){
+      linksStats.Broken ++; 
+    }
+  });
+  return linksStats;
+}
