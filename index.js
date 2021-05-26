f5c664aa3a9e4a42210729bcfaaf2462957d792b
karinmelissa <karin.escobar@usach.cl>
2021-05-25 20:28:46 -0400

    feat: resolve path reading

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const findMdLinks = require('./lib/mdLinks.js');
-const pathJS = require('path');
+const path = require('path');
 
 //getting the route & validate request from the console 
 let routeFile = process.argv[2];
 let validateLinks = process.argv[3];
 //resolves a sequence of paths or path segments into an absolute path.
-routeFile = pathJS.resolve(routeFile);
-//normalizes the given pathfixing writtings errors 
-routeFile = pathJS.normalize(routeFile);
+routeFile = path.resolve(routeFile);
+//normalizes the given path, fixing writtings errors 
+routeFile = path.normalize(routeFile);
 
 findMdLinks.mdLinks(routeFile,validateLinks);
