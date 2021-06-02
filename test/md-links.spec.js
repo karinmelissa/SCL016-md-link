2a40300fafe1e586cedc6de71a0f57c4f8d5c038
karinmelissa <karin.escobar@usach.cl>
2021-06-02 08:49:55 -0400

    Testing

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,13 +1,5 @@
 const { mdLinks, isMdFile, readMdFile, getLinkStatus, getLinks, findPath } = require('../lib/index.js');
 
-describe('mdLinks', () => {
-
-  it('should...', () => {
-    console.log('FIX ME!');
-  });
-
-});
-
 // Get Links from File
 describe('readMdFile', () => {
   it('should be a function', () => {
@@ -34,17 +26,37 @@ describe('mdLinks', () => {
     expect(typeof mdLinks).toBe('function');
   });
   it('should resolve to an object when validate is true', () => {
-    return mdLinks("readmeTest.md", { validate: true }).then(finalResult => {
+    return mdLinks('readmeTest.md', { validate: true }).then(finalResult => {
       expect(typeof finalResult).toBe('object');
     })
-  })
+  });
   it('should resolve to an object when validate is false', () => {
-    return mdLinks("readmeTest.md", { validate: false }).then(finalResult => {
+    return mdLinks('readmeTest.md', { validate: false }).then(finalResult => {
       expect(typeof finalResult).toBe('object');
     })
-  })
+  });
   //revisar path 
+  /*it('should resolve to an object when validate is false', () => {
+    return mdLinks("C:/Users/Usurio/Desktop/ProyectosLaboratoria/SCL016-md-link", { validate: false }).then(finalResult => {
+      expect(typeof finalResult).toBe('object');
+    })
+  })*/
 });
 
-
+//Mock up for node-fetch
+jest.mock('node-fetch');  
+const fetch = require('node-fetch'); 
+describe('getLinkStatus',()=>{
+  it('should be a function', () => {
+    expect(typeof getLinkStatus).toBe('function');
+  });
+  it('should return ok and status code for a correct link', () => {
+     // mockReturnValue es un  metodo de node-fectch
+    fetch.mockReturnValue(Promise.resolve({ status: 200, statusText: 'ok' }));
+    getLinkStatus("https://www.google.com/").then(obj => {
+      expect(obj.status).toBe(200);
+      expect(obj.ok).toBe('ok');
+    });
+  });
+})
 
